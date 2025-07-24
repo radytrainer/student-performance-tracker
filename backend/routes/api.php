dffe8327757289368da26bc1f37e69dfdd9de77f
Liyaan66 <liya.an@student.passerellesnumeriques.org>
2025-07-24 14:39:35 +0700

    resolve conflict

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
-// use App\Http\Controllers\Api\SubjectController;
 use App\Http\Controllers\Api\SubjectController;
+// use App\Http\Controllers\Api\SubjectController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
