8ff736a2d39b395d67465d705336c13033bc5ace
Sophy Em <emsophy80@gmail.com>
2025-08-28 08:27:03 +0700

    averrage scores

--- frontend/src/views/student/FeedbackSurveys.vue
+++ frontend/src/views/student/FeedbackSurveys.vue
@@ -102,7 +102,7 @@
             </div>
             <div class="ml-5 w-0 flex-1">
               <dl>
-                <dt class="text-sm font-medium text-gray-500 truncate">Average Score</dt>
+                <dt class="text-sm font-medium text-gray-500 truncate">Average Scores</dt>
                 <dd class="text-lg font-semibold text-gray-900">
                   {{ stats.average_score ? stats.average_score.toFixed(1) : 'N/A' }}
                 </dd>
